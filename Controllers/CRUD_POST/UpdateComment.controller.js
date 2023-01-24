00779c2ccac76dc1a2971a890865170843ef2409
Rajendra <itsrajendrapatel@gmail.com>
2023-01-24 15:54:16 +0530

    Images Changed

--- Controllers/CRUD_POST/UpdateComment.controller.js
+++ Controllers/CRUD_POST/UpdateComment.controller.js
@@ -1,13 +1,12 @@
 const { PostModel } = require("../../Models/Post.model");
 
 const UpdateComment = async (req, res) => {
-  let id = req.params.id;
-  let data = req.body;
+  let postid = req.params.id;
+  let { comment, id } = req.body;
   try {
-    const Post = await PostModel.findById(id);
-    let updateComment = (Post.comment.comment = data);
-    PostModel.findByIdAndUpdate(id, updateComment);
-    res.send("Every thing is working fine.");
+    const Post = await PostModel.findById(postid);
+    await Post.comment.findByIdAndUpdate(id, { comment });
+    res.send({ message: "Comment Updated" });
   } catch (error) {
     req.send({ message: error });
   }
